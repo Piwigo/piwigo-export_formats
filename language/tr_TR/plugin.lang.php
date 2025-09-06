8efb1caaa7d807cc8c059de84e38427bdb412ec9
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-09-06 18:42:29 +0200

    Update tr_TR, thanks to katpatuka

---
+++ language/tr_TR/plugin.lang.php
@@ -0,0 +1,32 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Setting dimensions for both sides will allow you to crop and choose which part of your photo to export. This ensures it won’t be distorted.'] = 'Her iki taraf için boyutları ayarlamak, fotoğrafınızdan hangi kısmı kesip dışa aktaracağınıza karar vermenizi sağlayacaktır. Bu, fotoğrafın bozulmamasını garanti eder.';
+$lang['Show as'] = 'Gösterilecek şekil';
+$lang['Show/Hide'] = 'Göster/Gizle';
+$lang['The export will use this exact dimension for one side, while the other side will be adjusted to maintain the photo\'s original aspect ratio.'] = 'Dışa aktarma, bir taraf için bu tam boyutu kullanacakken, diğer taraf fotoğrafın orijinal en-boy oranını korumak için ayarlanacaktır.';
+$lang['You can only set the dimension for one side.'] = 'Boyutunu yalnızca bir taraf için ayarlayabilirsin.';
+$lang['AF_RESET'] = 'Reset';
+$lang['AND'] = 'VE';
+$lang['Add an export format'] = 'Dışa aktarma biçimi ekle';
+$lang['Allow a specific group'] = 'Belirli bir gruba izin ver';
+$lang['Allow all admins to download'] = 'Tüm yöneticilerin indirmesine izin ver';
+$lang['Are you sure you want to delete the "%s" button?'] = '"%s" düğmesi silmek istediğinizden emin misiniz?';
+$lang['Auto formats'] = 'Otomatik formatlar';
+$lang['Buttons above the photo'] = 'Fotoğrafın üstündeki düğmeler';
+$lang['Buttons below the photo'] = 'Fotoğrafın altındaki düğmeler';
+$lang['Buttons inside the informations block'] = 'Bilgi bloğunun içindeki düğmeler';
+$lang['Crop at custom dimensions'] = 'Özel boyutlarda kırp';
+$lang['Edit an export format'] = 'Bir dışa aktarma biçimini düzenleyin';
+$lang['Export formats'] = 'Biçimleri dışa aktar';
+$lang['If your photo is in landscape mode, the maximum dimension will be the width. If it’s in portrait mode, the maximum dimension will be the height. In both cases, the original aspect ratio will be preserved.'] = 'Eğer fotoğrafınız manzara modundaysa, maksimum boyut genişlik olacaktır. Eğer portre modundaysa, maksimum boyut yükseklik olacaktır. Her iki durumda da, orijinal en-boy oranı korunacaktır.';
+$lang['Image dimensions'] = 'Görüntü boyutları';
+$lang['Links in the download dropdown'] = 'İndirme açılır menüsündeki bağlantılar';
+$lang['None of your choices will ever distort your photo.'] = 'Seçimlerinizden hiçbiri fotoğrafınızı bozmayacaktır.';
+$lang['OR'] = 'VEYA';
+$lang['Set both maximum dimensions'] = 'Her iki maksimum boyutu ayarla';
+$lang['Set only one dimension'] = 'Sadece bir boyut ayarlayın';
