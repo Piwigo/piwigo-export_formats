31fab3f565959201b452e9ec41e11b4ed4a89a48
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-09-06 08:36:06 +0200

    Update he_IL, thanks to epifeny

---
+++ language/he_IL/plugin.lang.php
@@ -0,0 +1,32 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Setting dimensions for both sides will allow you to crop and choose which part of your photo to export. This ensures it won’t be distorted.'] = 'קביעת ממדים לשני הצדדים תאפשר לך לחתוך ולבחור איזה חלק של התמונה שלך לייצא. זה מבטיח שהיא לא תהיה מעוותת.';
+$lang['Show as'] = 'הצג כ';
+$lang['Show/Hide'] = 'הצג/הסתר';
+$lang['The export will use this exact dimension for one side, while the other side will be adjusted to maintain the photo\'s original aspect ratio.'] = 'היצוא ישתמש בממד זה בדיוק לצד אחד, בעוד שהצד השני יותאם כדי לשמור על יחס הממדים המקורי של התמונה.';
+$lang['You can only set the dimension for one side.'] = 'ניתן לקבוע ממד רק לצד אחד.';
+$lang['AF_RESET'] = 'איפוס';
+$lang['AND'] = 'וגם';
+$lang['Add an export format'] = 'הוסף פורמט יצוא';
+$lang['Allow a specific group'] = 'אפשר לקבוצה מסוימת';
+$lang['Allow all admins to download'] = 'אפשר לכל המנהלים להוריד';
+$lang['Are you sure you want to delete the "%s" button?'] = 'האם אתה בטוח שברצונך למחוק את הכפתור "%s"?';
+$lang['Auto formats'] = 'פורמטים אוטומטיים';
+$lang['Buttons above the photo'] = 'כפתורים מעל התמונה';
+$lang['Buttons below the photo'] = 'כפתורים מתחת לתמונה';
+$lang['Buttons inside the informations block'] = 'כפתורים בתוך בלוק המידע';
+$lang['Crop at custom dimensions'] = 'חיתוך למידות מותאמות אישית';
+$lang['Edit an export format'] = 'ערוך פורמט יצוא';
+$lang['Export formats'] = 'פורמטי יצוא';
+$lang['If your photo is in landscape mode, the maximum dimension will be the width. If it’s in portrait mode, the maximum dimension will be the height. In both cases, the original aspect ratio will be preserved.'] = 'אם התמונה שלך במצב אופקי, המידה המרבית תהיה הרוחב. אם היא במצב אנכי, המידה המרבית תהיה הגובה. בשני המקרים, ישמר יחס הגדלים המקורי.';
+$lang['Image dimensions'] = 'מידות התמונה';
+$lang['Links in the download dropdown'] = 'קישורים בתפריט ההורדה';
+$lang['None of your choices will ever distort your photo.'] = 'אף אחת מבחירותיך לא תעוות את התמונה שלך.';
+$lang['OR'] = 'או';
+$lang['Set both maximum dimensions'] = 'קבע את שתי המידות המרביות';
+$lang['Set only one dimension'] = 'קבע מידה אחת בלבד';
