0f2ef37f1d39b88468557c6019b81a9a2c6231ea
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-09-06 18:42:24 +0200

    Update ca_ES, thanks to Tirs

---
+++ language/ca_ES/plugin.lang.php
@@ -0,0 +1,32 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Export formats'] = 'Formats d\'exportació';
+$lang['If your photo is in landscape mode, the maximum dimension will be the width. If it’s in portrait mode, the maximum dimension will be the height. In both cases, the original aspect ratio will be preserved.'] = 'Si la foto està en mode de paisatge, la dimensió màxima serà l\'ample. Si està en mode de retrat, la dimensió màxima serà l\'alçada. En tots dos casos es preservarà la proporció original.';
+$lang['Image dimensions'] = 'Dimensions de la imatge';
+$lang['Links in the download dropdown'] = 'Enllaços al desplegable de descàrrega';
+$lang['None of your choices will ever distort your photo.'] = 'Cap de les vostres seleccions distorsionarà mai la foto.';
+$lang['OR'] = 'O';
+$lang['Set both maximum dimensions'] = 'Estableix les dues dimensions màximes';
+$lang['Set only one dimension'] = 'Estableix només una dimensió';
+$lang['Setting dimensions for both sides will allow you to crop and choose which part of your photo to export. This ensures it won’t be distorted.'] = 'Establir les dimensions per als dos costats us permetrà retallar i escollir quina part de la foto voleu exportar. Això garanteix que no patirà distorsions.';
+$lang['Show as'] = 'Mostra com';
+$lang['Show/Hide'] = 'Mostra/Amaga';
+$lang['The export will use this exact dimension for one side, while the other side will be adjusted to maintain the photo\'s original aspect ratio.'] = 'L\'exportació utilitzarà aquesta dimensió exacta per a un costat, mentre que l\'altre s\'ajustarà per mantenir la relació d\'aspecte original de la foto.';
+$lang['You can only set the dimension for one side.'] = 'Només podeu definir la dimensió per a un costat.';
+$lang['AND'] = 'I';
+$lang['Add an export format'] = 'Afegeix un format d\'exportació';
+$lang['Allow a specific group'] = 'Permet un grup específic';
+$lang['Allow all admins to download'] = 'Permet que tots els administradors descarreguin';
+$lang['Are you sure you want to delete the "%s" button?'] = 'Esteu segur que voleu suprimir el botó "%s"?';
+$lang['Auto formats'] = 'Formats automàtics';
+$lang['Buttons above the photo'] = 'Botons per sobre de la foto';
+$lang['Buttons below the photo'] = 'Botons per sota de la foto';
+$lang['Buttons inside the informations block'] = 'Botons dins el bloc d\'informacions';
+$lang['Crop at custom dimensions'] = 'Retalla a dimensions personalitzades';
+$lang['Edit an export format'] = 'Edita un format d\'exportació';
+$lang['AF_RESET'] = 'Restableix';
