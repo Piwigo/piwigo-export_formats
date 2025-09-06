92f87fd5fbb20b1000332b905bd8fe7203133587
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-09-06 18:42:21 +0200

    Update es_ES, thanks to Tirs

---
+++ language/es_ES/plugin.lang.php
@@ -0,0 +1,32 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['AF_RESET'] = 'Restablecer';
+$lang['AND'] = 'Y';
+$lang['Add an export format'] = 'Añadir un formato de exportación';
+$lang['Allow a specific group'] = 'Permitir un grupo específico';
+$lang['Allow all admins to download'] = 'Permitir que todos los administradores descarguen';
+$lang['Are you sure you want to delete the "%s" button?'] = '¿Seguro que desea suprimir el botón "%s"?';
+$lang['Auto formats'] = 'Formatos automáticos';
+$lang['Buttons above the photo'] = 'Botones por encima de la foto';
+$lang['Buttons below the photo'] = 'Botones por debajo de la foto';
+$lang['Buttons inside the informations block'] = 'Botones dentro del bloque de informaciones';
+$lang['Crop at custom dimensions'] = 'Recortar a dimensiones personalizadas';
+$lang['Edit an export format'] = 'Editar un formato de explotación';
+$lang['Export formats'] = 'Formatos de exportación';
+$lang['If your photo is in landscape mode, the maximum dimension will be the width. If it’s in portrait mode, the maximum dimension will be the height. In both cases, the original aspect ratio will be preserved.'] = 'Si la foto está en modo paisaje, la dimensión máxima será la anchura. Si está en modo retrato, la dimensión máxima será la altura. En ambos casos se preservará la proporción original.';
+$lang['Image dimensions'] = 'Dimensiones de la imagen';
+$lang['Links in the download dropdown'] = 'Enlaces al desplegable de descarga';
+$lang['None of your choices will ever distort your photo.'] = 'Ninguna de las selecciones distorsionará nunca la foto.';
+$lang['OR'] = 'O';
+$lang['Set both maximum dimensions'] = 'Establecer ambas dimensiones máximas';
+$lang['Set only one dimension'] = 'Establecer sólo una dimensión';
+$lang['Setting dimensions for both sides will allow you to crop and choose which part of your photo to export. This ensures it won’t be distorted.'] = 'Establecer las dimensiones para ambos lados le permitirá recortar y escoger qué parte de la foto desea exportar. Esto garantiza que no habrá distorsiones.';
+$lang['Show as'] = 'Mostrar como';
+$lang['Show/Hide'] = 'Mostrar/Ocultar';
+$lang['The export will use this exact dimension for one side, while the other side will be adjusted to maintain the photo\'s original aspect ratio.'] = 'La exportación utilizará esta dimensión exacta para uno de los lados, mientras que el otro se ajustará para mantener la relación de aspecto original de la foto.';
+$lang['You can only set the dimension for one side.'] = 'Sólo se puede definir la dimensión para un lado.';
