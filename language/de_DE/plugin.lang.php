0ef5c4c343baeda88da644ce6b22d1ea05d9aa7a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-09-06 18:42:26 +0200

    Update de_DE, thanks to slim

---
+++ language/de_DE/plugin.lang.php
@@ -0,0 +1,32 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Set both maximum dimensions'] = 'Beide maximalen Abmessungen festlegen';
+$lang['Set only one dimension'] = 'Nur eine Dimension festlegen';
+$lang['Show as'] = 'Anzeigen als';
+$lang['Show/Hide'] = 'Anzeigen/Ausblenden';
+$lang['You can only set the dimension for one side.'] = 'Sie können die Abmessung nur für eine Seite festlegen.';
+$lang['Image dimensions'] = 'Bildabmessungen';
+$lang['Links in the download dropdown'] = 'Links im Dropdown-Menü „Download“';
+$lang['None of your choices will ever distort your photo.'] = 'Keine Ihrer Auswahlen wird jemals Ihr Foto verzerren.';
+$lang['OR'] = 'ODER';
+$lang['Setting dimensions for both sides will allow you to crop and choose which part of your photo to export. This ensures it won’t be distorted.'] = 'Durch das Festlegen der Abmessungen für beide Seiten können Sie das Foto zuschneiden und auswählen, welcher Teil exportiert werden soll. So wird sichergestellt, dass es nicht verzerrt wird.';
+$lang['The export will use this exact dimension for one side, while the other side will be adjusted to maintain the photo\'s original aspect ratio.'] = 'Der Export verwendet genau diese Abmessungen für eine Seite, während die andere Seite angepasst wird, um das ursprüngliche Seitenverhältnis des Fotos beizubehalten.';
+$lang['If your photo is in landscape mode, the maximum dimension will be the width. If it’s in portrait mode, the maximum dimension will be the height. In both cases, the original aspect ratio will be preserved.'] = 'Wenn Ihr Foto im Querformat vorliegt, ist die maximale Abmessung die Breite. Wenn es im Hochformat vorliegt, ist die maximale Abmessung die Höhe. In beiden Fällen bleibt das ursprüngliche Seitenverhältnis erhalten.';
+$lang['Buttons below the photo'] = 'Schaltflächen unter dem Foto';
+$lang['Buttons inside the informations block'] = 'Schaltflächen innerhalb des Informationsblocks';
+$lang['Crop at custom dimensions'] = 'Auf benutzerdefinierte Abmessungen zuschneiden';
+$lang['Edit an export format'] = 'Exportformat bearbeiten';
+$lang['Export formats'] = 'Exportformate';
+$lang['Allow all admins to download'] = 'Allen Administratoren das Herunterladen erlauben';
+$lang['Are you sure you want to delete the "%s" button?'] = 'Möchten Sie die Schaltfläche „%s“ wirklich löschen?';
+$lang['Auto formats'] = 'Automatische Formatierungen';
+$lang['Buttons above the photo'] = 'Schaltflächen über dem Foto';
+$lang['AF_RESET'] = 'Zurücksetzen';
+$lang['AND'] = 'UND';
+$lang['Add an export format'] = 'Exportformat hinzufügen';
+$lang['Allow a specific group'] = 'Eine bestimmte Gruppe zulassen';
