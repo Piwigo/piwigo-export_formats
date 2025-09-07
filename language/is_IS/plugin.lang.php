35f18f80cc37f0428bd76f52f6541307e7cf392e
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-09-07 06:57:18 +0200

    Update is_IS, thanks to gudjonsi

---
+++ language/is_IS/plugin.lang.php
@@ -0,0 +1,32 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Setting dimensions for both sides will allow you to crop and choose which part of your photo to export. This ensures it won’t be distorted.'] = 'Með því að stilla stærðir beggja hliða geturðu klippt myndina og valið hvaða hluta hennar á að flytja út. Þetta tryggir að hún verði ekki aflöguð.';
+$lang['Show as'] = 'Sýna sem';
+$lang['Show/Hide'] = 'Sýna/Fela';
+$lang['The export will use this exact dimension for one side, while the other side will be adjusted to maintain the photo\'s original aspect ratio.'] = 'Útflutningurinn mun nota þessa nákvæmu vídd fyrir aðra hliðina, en hina hliðina verður leiðrétt til að viðhalda upprunalegu hlutfallshlutfalli myndarinnar.';
+$lang['You can only set the dimension for one side.'] = 'Þú getur aðeins stillt víddina fyrir eina hlið.';
+$lang['AF_RESET'] = 'Endursetja';
+$lang['AND'] = 'OG';
+$lang['Add an export format'] = 'Bæta við útflutningssniði';
+$lang['Allow a specific group'] = 'Leyfa tilteknum hópi';
+$lang['Allow all admins to download'] = 'Leyfa öllum stjórnendum að hlaða niður';
+$lang['Are you sure you want to delete the "%s" button?'] = 'Ertu viss um að þú viljir eyða "%s" hnappinum?';
+$lang['Auto formats'] = 'Sjálfvirk snið';
+$lang['Buttons above the photo'] = 'Hnappar fyrir ofan myndina';
+$lang['Buttons below the photo'] = 'Hnappar fyrir neðan myndina';
+$lang['Buttons inside the informations block'] = 'Hnappar inni í upplýsingablokkinni';
+$lang['Crop at custom dimensions'] = 'Skerið í sérsniðnum víddum';
+$lang['Edit an export format'] = 'Breyta útflutningssniði';
+$lang['Export formats'] = 'Útflutningssnið';
+$lang['If your photo is in landscape mode, the maximum dimension will be the width. If it’s in portrait mode, the maximum dimension will be the height. In both cases, the original aspect ratio will be preserved.'] = 'Ef myndin er lárétt er hámarksvíddin breiddin. Ef hún er lárétt er hámarksvíddin hæðin. Í báðum tilvikum verður upprunalega hlutfallshlutfallið varðveitt.';
+$lang['Image dimensions'] = 'Myndvíddir';
+$lang['Links in the download dropdown'] = 'Tenglar í niðurhalsvalmyndinni';
+$lang['None of your choices will ever distort your photo.'] = 'Ekkert af vali þínu mun nokkurn tímann skekkja myndina þína.';
+$lang['OR'] = 'EÐA';
+$lang['Set both maximum dimensions'] = 'Stilltu bæði hámarksvíddir';
+$lang['Set only one dimension'] = 'Stilltu aðeins eina vídd';
