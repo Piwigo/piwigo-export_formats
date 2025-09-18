fa1be3a6f6f28dd0cdb5a7e1ac22724d2907c6e0
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-09-18 19:12:51 +0200

    Update da_DK, thanks to Kaare

---
+++ language/da_DK/plugin.lang.php
@@ -0,0 +1,32 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['If your photo is in landscape mode, the maximum dimension will be the width. If it’s in portrait mode, the maximum dimension will be the height. In both cases, the original aspect ratio will be preserved.'] = 'Hvis dit fotografi er i landskabsformat, vil den maksimale dimension være bredden. Hvis det er i portrætformat, vil den maksimale dimension være højden. I begge tilfælde vil det oprindelige størrelsesforhold blive bevaret.';
+$lang['Setting dimensions for both sides will allow you to crop and choose which part of your photo to export. This ensures it won’t be distorted.'] = 'Opsætning af dimensioner for begge sider gør det muligt at beskære og vælge hvilken dele af fotografiet, der skal eksporteres. Det sikrer at det ikke bliver forvrænget.';
+$lang['Show as'] = 'Vis som';
+$lang['Show/Hide'] = 'Vis/skjul';
+$lang['The export will use this exact dimension for one side, while the other side will be adjusted to maintain the photo\'s original aspect ratio.'] = 'Eksporten anvender denne specifikke dimension på ene side, mens den anden side vil blive justeret for at bevare fotografiet oprindelige størrelsesforhold.';
+$lang['You can only set the dimension for one side.'] = 'Du kan kun opsætte dimensionen for den ene side';
+$lang['AF_RESET'] = 'Nulstil';
+$lang['AND'] = 'OG';
+$lang['Add an export format'] = 'Tilføj et eksportformat';
+$lang['Allow a specific group'] = 'Tillad en specifik gruppe';
+$lang['Allow all admins to download'] = 'Tillad at alle administratorer kan downloade';
+$lang['Are you sure you want to delete the "%s" button?'] = 'Er du sikker på at du vil slette knappen "%s"?';
+$lang['Auto formats'] = 'Autoformater';
+$lang['Buttons above the photo'] = 'Knapper over fotografiet';
+$lang['Buttons below the photo'] = 'Knapper under fotografiet';
+$lang['Buttons inside the informations block'] = 'Knapper inde i informationsblokken';
+$lang['Crop at custom dimensions'] = 'Beskær med skræddersyede dimensioner';
+$lang['Edit an export format'] = 'Rediger et eksportformat';
+$lang['Export formats'] = 'Eksportformater';
+$lang['Image dimensions'] = 'Billeddimensioner';
+$lang['Links in the download dropdown'] = 'Links download-dropdown\'en';
+$lang['None of your choices will ever distort your photo.'] = 'Ingen af dine valg vil nogensinde forvrænge et fotografi';
+$lang['OR'] = 'ELLER';
+$lang['Set both maximum dimensions'] = 'Opsæt begge maksimumsdimensioner';
+$lang['Set only one dimension'] = 'Opsæt kun en dimension';
