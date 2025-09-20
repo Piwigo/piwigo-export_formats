e7af25e21e1874d3e4527d74f60bd222b1392197
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-09-20 12:56:59 +0200

    Update it_IT, thanks to kinmen

---
+++ language/it_IT/plugin.lang.php
@@ -0,0 +1,32 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Show as'] = 'Mostra come';
+$lang['Show/Hide'] = 'Mostra/Nascondi';
+$lang['The export will use this exact dimension for one side, while the other side will be adjusted to maintain the photo\'s original aspect ratio.'] = 'L\'esportazione utilizzerà esattamente questa dimensione per un lato, mentre l\'altro lato verrà calcolato per mantenere le proporzioni originali della foto.';
+$lang['You can only set the dimension for one side.'] = 'Puoi impostare soltanto la dimensione di un lato ';
+$lang['AF_RESET'] = 'Reimpostazione';
+$lang['AND'] = 'E';
+$lang['Add an export format'] = 'Aggiungi un formato di esportazione';
+$lang['Allow a specific group'] = 'Consenti ad un gruppo specifico';
+$lang['Allow all admins to download'] = 'Consenti a tutti gli amministratori di scaricare';
+$lang['Are you sure you want to delete the "%s" button?'] = 'Sei sicuro di voler eliminare il pulsante "%s"?';
+$lang['Auto formats'] = 'Formati automatici';
+$lang['Buttons above the photo'] = 'Pulsanti sopra la foto';
+$lang['Buttons below the photo'] = 'Pulsanti sotto la foto';
+$lang['Buttons inside the informations block'] = 'Pulsanti all\'interno del blocco informazioni';
+$lang['Crop at custom dimensions'] = 'Ritaglia con dimensioni personalizzate';
+$lang['Edit an export format'] = 'Modifica un formato di esportazione';
+$lang['Export formats'] = 'Formati di esportazione';
+$lang['If your photo is in landscape mode, the maximum dimension will be the width. If it’s in portrait mode, the maximum dimension will be the height. In both cases, the original aspect ratio will be preserved.'] = 'Se la tua foto è in modalità orizzontale, la dimensione massima sarà la larghezza. Se è in modalità verticale, la dimensione massima sarà l\'altezza. In entrambi i casi, il rapporto d\'aspetto originale verrà mantenuto.';
+$lang['Links in the download dropdown'] = 'Link nel menu a discesa dello scaricamento';
+$lang['None of your choices will ever distort your photo.'] = 'Nessuna delle tue scelte distorcerà mai la tua foto.';
+$lang['OR'] = 'O';
+$lang['Set both maximum dimensions'] = 'Imposta entrambe le dimensioni massime';
+$lang['Set only one dimension'] = 'Imposta solo una dimensione';
+$lang['Setting dimensions for both sides will allow you to crop and choose which part of your photo to export. This ensures it won’t be distorted.'] = 'Impostare le dimensioni per entrambi i lati ti permetterà di ritagliare e scegliere quale parte della foto esportare. Questo garantisce che essa non venga distorta.';
+$lang['Image dimensions'] = 'Dimensioni immagine';
