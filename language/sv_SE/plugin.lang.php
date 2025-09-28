2c7dd56f40b350b1ca30619a7219194636508104
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-09-28 08:16:34 +0200

    Update sv_SE, thanks to PBobbenB

---
+++ language/sv_SE/plugin.lang.php
@@ -0,0 +1,32 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Setting dimensions for both sides will allow you to crop and choose which part of your photo to export. This ensures it won’t be distorted.'] = 'Genom att ange mått för båda sidorna kan du beskära och välja vilken del av fotot som ska exporteras. Detta säkerställer att det inte förvrängs.';
+$lang['Show as'] = 'Visa som';
+$lang['Show/Hide'] = 'Visa/göm';
+$lang['The export will use this exact dimension for one side, while the other side will be adjusted to maintain the photo\'s original aspect ratio.'] = 'Genom att ange mått för båda sidorna kan du beskära och välja vilken del av fotot som ska exporteras. Detta säkerställer att det inte förvrängs.';
+$lang['You can only set the dimension for one side.'] = 'Du kan endast sätta dimensionen för en sida.';
+$lang['AF_RESET'] = 'Återställ';
+$lang['AND'] = 'OCH';
+$lang['Add an export format'] = 'Lägg till ett exportformat';
+$lang['Allow a specific group'] = 'Tillåt en specifik grupp';
+$lang['Allow all admins to download'] = 'Tillåt alla administratörer att ladda ned';
+$lang['Are you sure you want to delete the "%s" button?'] = 'Är du säker du vill radera "%s" knappen?';
+$lang['Auto formats'] = 'Autoformat';
+$lang['Buttons above the photo'] = 'Knappar ovanför foto';
+$lang['Buttons below the photo'] = 'Knappar nedanför foto';
+$lang['Buttons inside the informations block'] = 'Knappar inne i informationsblocket';
+$lang['Crop at custom dimensions'] = 'Beskär till egna dimensioner';
+$lang['Edit an export format'] = 'Redigera ett exportformat';
+$lang['Export formats'] = 'Exportformat';
+$lang['If your photo is in landscape mode, the maximum dimension will be the width. If it’s in portrait mode, the maximum dimension will be the height. In both cases, the original aspect ratio will be preserved.'] = 'Om ditt foto är i liggande läge är det maximala måttet bredden. Om det är i stående läge är det maximala måttet höjden. I båda fallen bevaras det ursprungliga bildförhållandet.';
+$lang['Image dimensions'] = 'Bilddimensioner';
+$lang['Links in the download dropdown'] = 'Länkar i nedladdningsmenyn';
+$lang['None of your choices will ever distort your photo.'] = 'Inget av dina val kommer någonsin att förvränga ditt foto.';
+$lang['OR'] = 'ELLER';
+$lang['Set both maximum dimensions'] = 'Ställ in båda maximala måtten';
+$lang['Set only one dimension'] = 'Ställ in endast en dimension';
