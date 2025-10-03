ddea66c346a5e4e02313d9c4d0688b9b4f8e4d4a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-10-03 05:46:55 +0200

    Update nl_NL, thanks to pauldaytona

---
+++ language/nl_NL/plugin.lang.php
@@ -0,0 +1,32 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Buttons below the photo'] = 'Knoppen onder de afbeelding';
+$lang['Allow a specific group'] = 'Geef een bepaalde groep toegang';
+$lang['If your photo is in landscape mode, the maximum dimension will be the width. If it’s in portrait mode, the maximum dimension will be the height. In both cases, the original aspect ratio will be preserved.'] = 'Als uw foto in de liggende modus staat, is de maximale afmeting de breedte. Als uw foto in de staande modus staat, is de maximale afmeting de hoogte. In beide gevallen blijft de oorspronkelijke beeldverhouding behouden.';
+$lang['Setting dimensions for both sides will allow you to crop and choose which part of your photo to export. This ensures it won’t be distorted.'] = 'Door de afmetingen voor beide zijden in te stellen, kunt u bijsnijden en kiezen welk deel van uw foto u wilt exporteren. Zo voorkomt u dat de foto vervormd raakt.';
+$lang['The export will use this exact dimension for one side, while the other side will be adjusted to maintain the photo\'s original aspect ratio.'] = 'Bij het exporteren worden deze exacte afmetingen voor één zijde gebruikt, terwijl de andere zijde wordt aangepast om de oorspronkelijke beeldverhouding van de foto te behouden.';
+$lang['Allow all admins to download'] = 'Laat alle admins downloaden';
+$lang['Crop at custom dimensions'] = 'Verklein naar aangepaste maten.';
+$lang['None of your choices will ever distort your photo.'] = 'Geen van je keuzes zal de foto vervormen.';
+$lang['Set both maximum dimensions'] = 'Stel beide maximum maten in.';
+$lang['Are you sure you want to delete the "%s" button?'] = 'Weet je zeker dat je de "%s" knop wil verwijderen?';
+$lang['Auto formats'] = 'Automatische formaten';
+$lang['Buttons above the photo'] = 'Knoppen boven de afbeelding';
+$lang['Buttons inside the informations block'] = 'Knoppen binnen het informatie blok';
+$lang['Links in the download dropdown'] = 'Links in de download ';
+$lang['Set only one dimension'] = 'Stel één formaat in.';
+$lang['Show as'] = 'Toon als';
+$lang['Show/Hide'] = 'Toon/Verberg';
+$lang['You can only set the dimension for one side.'] = 'Je kunt alleen de afmeting voor één kant instellen.';
+$lang['AF_RESET'] = 'Herstel';
+$lang['AND'] = 'EN';
+$lang['Add an export format'] = 'Voeg een export formaat toe';
+$lang['Edit an export format'] = 'Bewerk een export formaat';
+$lang['Export formats'] = 'Export formaten';
+$lang['Image dimensions'] = 'Afbeelding maten';
+$lang['OR'] = 'OF';
