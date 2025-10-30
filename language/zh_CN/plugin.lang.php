61a761e4d052050c703e992f7482b006ddf8f83e
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-10-30 19:01:58 +0100

    Update zh_CN, thanks to walle

---
+++ language/zh_CN/plugin.lang.php
@@ -0,0 +1,32 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Setting dimensions for both sides will allow you to crop and choose which part of your photo to export. This ensures it won’t be distorted.'] = '同时设置宽度和高度：您可对照片进行裁剪并选择要导出的部分。这能确保导出的照片不会变形。';
+$lang['Show as'] = '显示为';
+$lang['Show/Hide'] = '显示/隐藏';
+$lang['The export will use this exact dimension for one side, while the other side will be adjusted to maintain the photo\'s original aspect ratio.'] = '导出时，会精确使用您设置的这一侧尺寸，另一侧尺寸将按照片原始宽高比自动调整。';
+$lang['You can only set the dimension for one side.'] = '您只能设置单侧的尺寸。';
+$lang['AF_RESET'] = '重置';
+$lang['AND'] = '和';
+$lang['Add an export format'] = '添加导出格式';
+$lang['Allow a specific group'] = '允许特定用户组';
+$lang['Allow all admins to download'] = '允许所有管理员下载';
+$lang['Are you sure you want to delete the "%s" button?'] = '您确定要删除"%s" 按钮吗？';
+$lang['Auto formats'] = '自动格式';
+$lang['Buttons above the photo'] = '照片上方的按钮';
+$lang['Buttons below the photo'] = '照片下方的按钮';
+$lang['Buttons inside the informations block'] = '信息块内的按钮';
+$lang['Crop at custom dimensions'] = '按自定义尺寸裁剪';
+$lang['Edit an export format'] = '编辑导出格式';
+$lang['Export formats'] = '导出格式';
+$lang['If your photo is in landscape mode, the maximum dimension will be the width. If it’s in portrait mode, the maximum dimension will be the height. In both cases, the original aspect ratio will be preserved.'] = '若您的照片为横版（景观模式），最大尺寸将为宽度；若为竖版（人像模式），最大尺寸将为高度。两种情况下均会保持原始宽高比。';
+$lang['Image dimensions'] = '图片尺寸';
+$lang['Links in the download dropdown'] = '下载下拉菜单中的链接';
+$lang['None of your choices will ever distort your photo.'] = '您的任何选择都不会导致照片变形。';
+$lang['OR'] = '或';
+$lang['Set both maximum dimensions'] = '设置最大宽度和高度';
+$lang['Set only one dimension'] = '仅设置一个尺寸';
