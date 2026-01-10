c27b61290f60cc9efd7470ee3e5b8604fba1994c
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-01-10 10:37:41 +0100

    Update pl_PL, thanks to Sauron

---
+++ language/pl_PL/plugin.lang.php
@@ -0,0 +1,26 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['AF_RESET'] = 'Reset';
+$lang['AND'] = 'I';
+$lang['Add an export format'] = 'Dodaj format eksportu';
+$lang['Allow a specific group'] = 'Zezwalaj konkretnej grupie';
+$lang['Allow all admins to download'] = 'Zezwalaj wszystkim administratorom na pobieranie';
+$lang['Are you sure you want to delete the "%s" button?'] = 'Czy na pewno chcesz skasować przycisk "%s"?';
+$lang['Auto formats'] = 'Format automatyczny';
+$lang['Buttons above the photo'] = 'Przyciski nad zdjęciem';
+$lang['Buttons below the photo'] = 'Przyciski pod zdjęciem';
+$lang['Buttons inside the informations block'] = 'Przyciski w bloku informacyjnym';
+$lang['Crop at custom dimensions'] = 'Przytnij do wybranego rozmiaru';
+$lang['Edit an export format'] = 'Edytuje format eksportu';
+$lang['Export'] = 'Eksport';
+$lang['Export formats'] = 'Formaty do eksportu';
+$lang['Image dimensions'] = 'Wymiary obrazu';
+$lang['Links in the download dropdown'] = 'Linki w menu download';
+$lang['None of your choices will ever distort your photo.'] = 'Żadne z twoich wyborów nie zniszczy twojego zdjęcia.';
+$lang['OR'] = 'LUB';
+$lang['Set both maximum dimensions'] = 'Usta oba maksymalne wymiary';
