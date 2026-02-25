58d5d9189534102f6e5b4e8460cf376307fb459e
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-02-25 14:03:02 +0100

    Update ko_KR, thanks to yjchoi

---
+++ language/ko_KR/plugin.lang.php
@@ -0,0 +1,33 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Set only one dimension'] = '한쪽 크기만 설정';
+$lang['Setting dimensions for both sides will allow you to crop and choose which part of your photo to export. This ensures it won’t be distorted.'] = '양쪽 크기를 설정하면 사진을 자르고 내보낼 부분을 선택할 수 있습니다. 이렇게 하면 사진이 왜곡되지 않습니다.';
+$lang['Show as'] = '표시 방식';
+$lang['Show/Hide'] = '표시/숨김';
+$lang['The export will use this exact dimension for one side, while the other side will be adjusted to maintain the photo\'s original aspect ratio.'] = '내보낼 때 한쪽은 정확히 이 크기를 사용하며, 다른 쪽은 사진의 원래 종횡비를 유지하도록 조정됩니다.';
+$lang['You can only set the dimension for one side.'] = '한쪽의 크기만 설정할 수 있습니다.';
+$lang['AF_RESET'] = '초기화';
+$lang['AND'] = 'AND';
+$lang['Add an export format'] = '내보내기 형식 추가';
+$lang['Allow a specific group'] = '특정 그룹 허용';
+$lang['Allow all admins to download'] = '모든 관리자 다운로드 허용';
+$lang['Are you sure you want to delete the "%s" button?'] = '"%s" 버튼을 삭제하시겠습니까?';
+$lang['Auto formats'] = '자동 형식';
+$lang['Buttons above the photo'] = '사진 위 버튼';
+$lang['Buttons below the photo'] = '사진 아래 버튼';
+$lang['Buttons inside the informations block'] = '정보 블록 내 버튼';
+$lang['Crop at custom dimensions'] = '사용자 지정 크기로 자르기';
+$lang['Edit an export format'] = '내보내기 형식 편집';
+$lang['Export'] = '내보내기';
+$lang['Export formats'] = '내보내기 형식';
+$lang['If your photo is in landscape mode, the maximum dimension will be the width. If it’s in portrait mode, the maximum dimension will be the height. In both cases, the original aspect ratio will be preserved.'] = '사진이 가로 모드인 경우 최대 크기는 너비가 됩니다. 세로 모드인 경우 최대 크기는 높이가 됩니다. 두 경우 모두 원본 화면 비율이 유지됩니다.';
+$lang['Image dimensions'] = '이미지 크기';
+$lang['Links in the download dropdown'] = '다운로드 드롭다운 메뉴의 링크';
+$lang['None of your choices will ever distort your photo.'] = '선택하신 옵션은 사진의 왜곡을 절대 일으키지 않습니다.';
+$lang['OR'] = 'OR';
+$lang['Set both maximum dimensions'] = '최대 크기를 모두 설정';
